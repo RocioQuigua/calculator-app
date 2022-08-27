1124f352d6056f65024fc58265ff67f49b66a12c
Rocio Quigua <rocioquigualopez@gmail.com>
2022-08-26 23:09:13 -0500

    feat(organisms): update

---
+++ src/components/organisms/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Change } from '../../atoms/Change/Change';
+
+export const Header = () => {
+  return (
+    <div className='header'>
+      <Change />
+    </div>
+  );
+};
---
+++ src/components/organisms/MenuOptions/MenuOptions.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Buttons } from '../../atoms/Buttons/Buttons';
+
+export const MenuOptions = () => {
+  return (
+    <div>
+      <Buttons />
+    </div>
+  );
+};
