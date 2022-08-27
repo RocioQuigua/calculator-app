1cd6f5620fad1179c4e30be871ffca188df44d74
Rocio Quigua <rocioquigualopez@gmail.com>
2022-08-26 23:09:52 -0500

    feat(atoms): update

---
+++ src/components/atoms/Buttons/Buttons.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Button } from 'antd';
+
+export const Buttons = () => {
+  return(
+    <div className='buttons'>
+      <Button></Button>
+    </div>
+  );
+};
---
+++ src/components/atoms/InputResult/InputResul.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Input } from 'antd';
+
+export const InputResult = () => {
+  return (
+    <div className='inputResult'>
+      <Input />
+    </div>
+  );
+};
