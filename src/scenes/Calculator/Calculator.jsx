6a997f59fadb4a5f0dd8af29d728ebc25d8297d1
Rocio Quigua <rocioquigualopez@gmail.com>
2022-09-16 18:10:59 -0500

    feat(scenes): update

---
+++ src/scenes/Calculator/Calculator.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { InputResult } from '../../components/organisms/InputResult/InputResul';
+import { Header } from '../../components/organisms/Header/Header';
+import { KeyboardCalculator } from '../../components/organisms/KeyboardCalculator/KeyboardCalculator';
+
+export const Calculator = () => {
+  return (
+    <div className='calculator'>
+      <Header />
+      <InputResult />
+      <KeyboardCalculator />
+    </div>
+  );
+};
