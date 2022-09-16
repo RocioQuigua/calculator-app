af3acbb29d637b25ce4bd6b6392bb36cedab16cd
Rocio Quigua <rocioquigualopez@gmail.com>
2022-09-16 18:12:11 -0500

    feat(js): update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/principal/App';
+import App from './scenes/App';
 import reportWebVitals from './reportWebVitals';
+
+import "./sass/main.scss";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/scenes/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Calculator } from './Calculator/Calculator';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
