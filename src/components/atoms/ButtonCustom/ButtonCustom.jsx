2b302a1eda28c4e70b1a724f77f2851b64b11133
Rocio Quigua <rocioquigualopez@gmail.com>
2022-09-18 23:58:33 -0500

    feat(ButtonCustom): update

--- src/components/atoms/ButtonCustom/ButtonCustom.jsx
+++ src/components/atoms/ButtonCustom/ButtonCustom.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Button } from 'antd';
+import PropTypes from 'prop-types';
 
-export const ButtonCustom = () => {
+export const ButtonCustom = ({name, onClick, type}) => {
   return (
-    <div>
-    
-    </div>
+    <Button className={`button-custom button-custom--${type}`} onClick={onClick}>{name}</Button>
   );
+};
+
+ButtonCustom.propTypes = {
+  key: PropTypes.any,
+  name: PropTypes.string,
+  onClick: PropTypes.string,
+  type: PropTypes.string
 };
