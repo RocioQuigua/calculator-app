6f046c409c6f0633b6f7cb71373b1f6fba23ff82
Rocio Quigua <rocioquigualopez@gmail.com>
2022-09-20 23:21:31 -0500

    feat(keyboardItems): item separation in refractor

---
+++ src/common/utils/keyboardItems.js
@@ -0,0 +1,95 @@
+export const items = [
+  {
+    key: 7,
+    name: '7',
+    type: 'default',
+  },
+  {
+    key: 8,
+    name: '8',
+    type: 'default',
+  },
+  {
+    key: 9,
+    name: '9',
+    type: 'default',
+  },
+  {
+    key: 'DEL',
+    name: 'DEL',
+    type: 'primary',
+  },
+  {
+    key: 4,
+    name: '4',
+    type: 'default',
+  },
+  {
+    key: 5,
+    name: '5',
+    type: 'default',
+  },
+  {
+    key: 6,
+    name: '6',
+    type: 'default',
+  },
+  {
+    key: '+',
+    name: '+',
+    type: 'default',
+  },
+  {
+    key: 1,
+    name: '1',
+    type: 'default',
+  },
+  {
+    key: 2,
+    name: '2',
+    type: 'default',
+  },
+  {
+    key: 3,
+    name: '3',
+    type: 'default',
+  },
+  {
+    key: '-',
+    name: '-',
+    type: 'default',
+  },
+  {
+    key: '.',
+    name: '.',
+    type: 'default',
+  },
+  {
+    key: 0,
+    name: '0',
+    type: 'default',
+  },
+  {
+    key: '/',
+    name: '/',
+    type: 'default',
+  },
+  {
+    key: 'x',
+    name: 'x',
+    type: 'default',
+  },
+];
+
+export const actions = [
+  {
+    key: 'RESET',
+    name: 'RESET',
+    type: 'primary',
+  },
+  {
+    key: '=',
+    name: '=',
+    type: 'secundary',
+  },
+];
