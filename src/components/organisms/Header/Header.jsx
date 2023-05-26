73e61cef536f28a6b1ec5fe3c597381feb214129
Rocio Quigua <rocioquigualopez@gmail.com>
2023-05-25 23:39:35 -0500

    feat(Header): added global state

--- src/components/organisms/Header/Header.jsx
+++ src/components/organisms/Header/Header.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { SwitchTheme } from '../../organisms/SwitchTheme/SwitchTheme';
 
+import {useStore } from '../../../store/useStore';
+
 export const Header = () => {
+  const theme = useStore((state) => state.theme);
   return (
-    <div className='header'>
-      <h1>calc</h1>
+    <div className={`header header--${theme}`}>
+      <h1 className={`title title--${theme}`}>calc</h1>
       <SwitchTheme themes={3}/>
     </div>
   );
