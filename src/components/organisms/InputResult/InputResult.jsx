0e92f3f2be4e10e8b91e91743e0bc95d97ef4fa8
Rocio Quigua <rocioquigualopez@gmail.com>
2023-05-25 23:40:06 -0500

    feat(InputResult): added global state

--- src/components/organisms/InputResult/InputResult.jsx
+++ src/components/organisms/InputResult/InputResult.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useStore } from '../../../store/useStore';
 
 export const InputResult = ({value, /*setValue*/}) => {
+  const theme = useStore((state) => state.theme);
   return (
-    <div className='input-result'>
-      <input value={value}  readOnly />
+    <div className={`input-result input-result--${theme}`}>
+      <input className={`result result--${theme}`} value={value}  readOnly />
     </div>
   );
 };
