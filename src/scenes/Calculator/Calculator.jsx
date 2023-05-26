92fb05ee48e315fbe7454e26ccacff684506317d
Rocio Quigua <rocioquigualopez@gmail.com>
2023-05-25 23:43:18 -0500

    feat(Calculator): added global state

--- src/scenes/Calculator/Calculator.jsx
+++ src/scenes/Calculator/Calculator.jsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 import { InputResult } from '../../components/organisms/InputResult/InputResult';
 import { Header } from '../../components/organisms/Header/Header';
 import { KeyboardCalculator } from '../../components/organisms/KeyboardCalculator/KeyboardCalculator';
+import { useStore } from '../../store/useStore';
 
 export const Calculator = () => {
   const [valueInput, setValueInput] = useState('');
+  const theme = useStore((state) => state.theme);
 
   return (
-    <div className='calculator'>
+    <div className={`calculator calculator--${theme}`}>
       <div className='calculator__container'>
         <Header />
         <InputResult value={valueInput} setValue={setValueInput}/>
