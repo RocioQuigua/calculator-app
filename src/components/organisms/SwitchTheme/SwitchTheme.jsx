9bae5a76e5c2418b14d3933b620c9632e8bb9d45
Rocio Quigua <rocioquigualopez@gmail.com>
2023-05-25 23:42:35 -0500

    feat(SwitchTheme): added global state

--- src/components/organisms/SwitchTheme/SwitchTheme.jsx
+++ src/components/organisms/SwitchTheme/SwitchTheme.jsx
@@ -2,21 +2,23 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Switch } from '../../atoms/Switch/Switch';
+import { useStore } from '../../../store/useStore';
 
 export const SwitchTheme = ({ themes }) => {
-  const [theme, setTheme] = useState(1);
+  const [themeSel, setThemeSel] = useState(1);
   const numbers = new Array(themes);
+  const theme = useStore((state) => state.theme);
 
   return (
-    <div className="switch-theme">
-      <h1>THEME</h1>
+    <div className={`switch-theme switch-theme--${theme}`}>
+      <h1 className={`switch-theme__title switch-theme__title--${theme}`}>THEME</h1>
       <div className="switch-theme__content">
-        <div className="switch-theme__numbers">
+        <div className='switch-theme__numbers'>
           {[...numbers].map((_, index) => (
             <p key={index}>{index + 1}</p>
           ))}
         </div>
-        <Switch size={themes} active={theme} setActive={setTheme}/>
+        <Switch size={themes} active={themeSel} setActive={setThemeSel}/>
       </div>
     </div>
   );
